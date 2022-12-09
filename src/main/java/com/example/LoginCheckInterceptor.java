800e1d0a358228935410191a65782ffc0c88ffd3
Youngkwan-Cho <dudrhks1009@naver.com>
2022-12-09 17:19:53 +0900

    change codes

--- src/main/java/com/example/LoginCheckInterceptor.java
+++ src/main/java/com/example/LoginCheckInterceptor.java
@@ -1,13 +1,12 @@
 package com.example;
 
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginCheckInterceptor implements HandlerInterceptor {
+public class LoginCheckInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -18,15 +17,5 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
             return false;
         }
         return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-
     }
 }
